cab8c1ae9c2a37f1703360eed5feae54f7528174
ali said <aliasy19932017@gmail.com>
2024-12-30 19:18:06 +0200

    first commit created pro 3 tailwind

---
+++ js/main.js
@@ -0,0 +1,70 @@
+let countDownDate = new Date("Dec 31, 2025 23:59:59").getTime();
+
+let counter = setInterval(() => {
+  // Get Date Now
+  let dateNow = new Date().getTime();
+  // Find The Date Difference Between Now And Countdown Date
+  let dateDiff = countDownDate - dateNow;
+
+  // Get Time Units
+  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
+  document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+  document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+
+  if (dateDiff < 0) {
+    clearInterval(counter);
+  }
+}, 1000);
+
+
+
+let progressSpans = document.querySelectorAll(".the-progress span");
+let section = document.querySelector(".our-skills");
+let nums = document.querySelectorAll(".stats .number");
+let statsSection = document.querySelector(".stats");
+let started = false; 
+
+window.onscroll = function () {
+  // Skills Animate Width
+  if (window.scrollY >= section.offsetTop - 250) {
+    progressSpans.forEach((span) => {
+      span.style.width = span.dataset.width;
+    });
+  }
+  // Stats Increase Number
+  if (window.scrollY >= statsSection.offsetTop ) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
+    }
+    started = true;
+  }
+  // Navigation scroll to up with btn
+  if (window.scrollY >= 200) {
+    document.querySelector(".btn-scroll-top").style.display = "block";
+    document.querySelector(".btn-scroll-top").style.opacity = "1";
+  } else {
+    document.querySelector(".btn-scroll-top").style.display = "none";
+    document.querySelector(".btn-scroll-top").style.opacity = "0";
+  }
+};
+
+function startCount(el) {
+  let goal = el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
+
+document.querySelector(".btn-scroll-top").addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,104 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    container :{
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      colors: {
+        primary: "#2196f3",
+        secondary: "#1787e0",
+        backsection: "#ececec",
+        facebook: '#1877f2',
+        twitter: '#1da1f2',
+        youtube: '#ff0000',
+        footer : '#b9b9b9'
+      },
+      fontFamily: {
+        cairo: ['Cairo', 'sans-serif'],
+      },
+      animation: {
+        'up-and-down': 'up-and-down 5s linear infinite',
+        'moving-arrow': 'moving-arrow 0.6s linear infinite',
+        'left-move': 'left-move 0.5s linear forwards',
+        'right-move': 'right-move 0.5s linear forwards',
+        'flashing': 'flashing 0.7s forwards',
+        'changeBack': 'changeBackground 10s linear infinite',
+      },
+      keyframes: {
+        'up-and-down': {
+          '0%, 100%': {
+            transform: 'translateY(0)',
+          },
+          '50%': {
+            transform: 'translateY(-50px)', 
+          },
+        },
+        'moving-arrow': {
+          '100%': {
+            transform: 'translateX(10px)',
+          },
+        },
+        'left-move': {
+          '50%': {
+            left: '0',
+            width: '12px',
+            height: '12px',
+          },
+          '100%': {
+            left: '0',
+            width: '50%',
+            height: '100%',
+            borderRadius: '0',
+          },
+        },
+        'right-move': {
+          '50%': {
+            right: '0',
+            width: '12px',
+            height: '12px',
+          },
+          '100%': {
+            right: '0',
+            width: '51%',
+            height: '100%',
+            borderRadius: '0',
+          },
+        },
+        'flashing': {
+          '0%, 40%': {
+            opacity: '1',
+          },
+          '100%': {
+            opacity: '0',
+            width: '200%',
+            height: '200%',
+          },
+        },
+        'changeBackground': {
+          '0%, 100%': {
+            'background-image': "url('../imgs/discount-background1.jpg')",
+          },
+          '50%': {
+            'background-image': "url('../imgs/discount-background2.jpg')",
+          },
+        },
+      },
+      boxShadow: {
+        'custom': '0 12px 20px 0 rgba(0, 0, 0, 0.13), 0 2px 4px 0 rgba(0, 0, 0, 0.12)',
+      }
+    },
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.counter-services': {
+          'counter-increment': 'services',
+        }
+      }, ['responsive', 'hover']);
+    }
+  ]
+}
+
